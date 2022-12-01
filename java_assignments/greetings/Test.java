12ec23f296e1e665ad6001b346932a47cd7c25f5
BuddyReed <reedgreg86@gmail.com>
2022-12-01 13:57:17 -0800

    Java_Stack

---
+++ java_assignments/greetings/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args){
+    // Key words like Function | main is the method | is parameter used for the outscope.
+        System.out.println("Hello World");
+    }
+}

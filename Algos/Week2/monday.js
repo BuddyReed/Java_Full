9161e4ed7552905c702cfa7c266f4673daaaaf77
BuddyReed <reedgreg86@gmail.com>
2022-12-09 11:58:35 -0800

    Update

--- Algos/Week2/monday.js
+++ Algos/Week2/monday.js
@@ -229,6 +229,56 @@ class BinarySearchTree {
         // RETURN ARRAY OF NUMBERS
         // CALL STACKING WHEN GOING DOWN RECUSSIVE
     }
+
+
+// ------------------- FRIDAY --------------------------//
+    /**
+     * Recursively counts the total number of nodes in this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during the traversal of this tree.
+     * @returns {number} The total number of nodes.
+     */
+    size(node = this.root) {
+      // BASE CASE
+    if(node === null){
+        return 0
+    }
+    return 1 + this.size(node.left) + this.size(node.right)
+    }
+
+    /**
+     * Calculates the height of the tree which is based on how many nodes from
+     * top to bottom (whichever side is taller).
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during traversal of this tree.
+     * @returns {number} The height of the tree.
+     */
+    height(node = this.root) {
+      // BASE CASE
+    if(node === null){
+        return 0
+    }
+      // COMPARE THE LEFT HEIGHT AND RIGHT HEIGHT
+      // WHICHEVER HEIGHT IS BIGGER ADD ONE MORE TO THAT HEIGHT
+    }
+
+    /**
+     * Determine whether the BST is balanced, for a BST to be balanced,
+     * a Node's left and right height must have a max difference of 1 AND
+     * all Nodes in the tree must be balanced as well.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during traversal of this tree.
+     * @returns {number} The height of the tree.
+     */
+    isBalanced(node = this.root) {
+    
+    }
+
+
+
 }
 
 // Wednesday ------------------------------------------Thursday-----------
